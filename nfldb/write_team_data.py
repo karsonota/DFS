d3b9cec891a078432c0a01a4729ccfcede9c441b
backdoorbreaker <12wanga@gmail.com>
2017-11-04 17:06:49 -0700

    Team and Player nfldb scripts. See nfldb/outputs

--- nfldb/write_team_data.py
+++ nfldb/write_team_data.py
@@ -14,10 +14,14 @@ teams = ['NE','NYJ','MIA','BUF','PIT','BAL','CLE','CIN','HOU','IND',
          'JAC','TEN','DEN','KC','SD','OAK',
          'DAL','WAS','NYG','PHI','MIN','GB','DET','CHI',
          'NO','TB','ATL','CAR','SF','SEA','ARI','STL'] 
-years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
 
 for year in years:
-    with open('output/team_stats_'+str(year)+'.csv', 'wb') as csvfile:
+    if year == 2016:
+        teams = ['LA' if x=='STL' else x for x in teams]
+    elif year == 2017:
+        teams = ['LAC' if x=='SD' else x for x in teams]
+    with open('output/team/team_off_stats_'+str(year)+'.csv', 'wb') as csvfile:
         c = csv.writer(csvfile)
         c.writerow(['Team','Total Pass Yards','Total Pass TDs','Total Pass Att',
                     'Total Rush Yards','Total Rush TDs', 'Total Rush Att'])
@@ -31,3 +35,19 @@ for year in years:
             c.writerow([team, pYds, pTds, pAtt, rYds, rTds, rAtt])
         csvfile.close()
         
+for year in years:
+    if year == 2016:
+        teams = ['LA' if x=='STL' else x for x in teams]
+    elif year == 2017:
+        teams = ['LAC' if x=='SD' else x for x in teams]
+    with open('output/team/team_def_stats_'+str(year)+'.csv', 'wb') as csvfile:
+        c = csv.writer(csvfile)
+        c.writerow(['Team', 'Total Pass Yards Allowed', 'Total Pass TDs Allowed',
+                    'Total Rush Yards Allowed', 'Total Rush TDs Allowed'])
+        for team in teams:
+            pYdsA = str(ts.team_passing_yds_all(year, team))
+            pTdsA = str(ts.team_passing_tds_all(year, team))
+            rYdsA = str(ts.team_rushing_yds_all(year, team))
+            rTdsA = str(ts.team_rushing_tds_all(year, team))
+            c.writerow([team, pYdsA, pTdsA, rYdsA, rTdsA])
+        csvfile.close()
---
+++ vegas_lines/espn_scraper.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 04 12:13:48 2017
+
+@author: Alan
+Scrape espn.com/nfl/lines for weekly over unders and spread
+"""
+
